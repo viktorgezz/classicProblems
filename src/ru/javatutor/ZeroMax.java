6a67484523daa95c31210cf5bd7d8b66a3d62f08
AndreyVerov <viktorgezz@mail.ru>
2023-06-22 17:58:08 +0300

    2

---
+++ src/ru/javatutor/ZeroMax.java
@@ -0,0 +1,21 @@
+package ru.javatutor;
+
+import javax.sound.midi.Soundbank;
+
+public class ZeroMax {
+
+    public int[] zeroMax(int[] nums) {
+        int maxOdd = Integer.MIN_VALUE;
+        boolean wasOdd = false;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] % 2 != 0) {
+                maxOdd = Math.max(nums[i], maxOdd);
+                wasOdd = true;
+            }
+            if (nums[i] == 0 && wasOdd) {
+                nums[i] = maxOdd;
+            }
+        }
+        return nums;
+    }
+}
