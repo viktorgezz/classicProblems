7984e24736004cab0b333eb2df3ea3de96a3e2f1
AndreyVerov <viktorgezz@mail.ru>
2023-06-22 17:57:19 +0300

    1

---
+++ src/ru/javatutor/FindFirstPalindromic.java
@@ -0,0 +1,21 @@
+package ru.javatutor;
+
+public class FindFirstPalindromic {
+    public String firstPalindrome(String[] words) {
+        for (String word : words) {
+            if (isPalindrome(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+
+    private boolean isPalindrome(String word) {
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
