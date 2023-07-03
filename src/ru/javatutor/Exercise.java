0f23b462145606e334a25ea099f13bc0353f338b
AndreyVerov <viktorgezz@mail.ru>
2023-07-03 17:13:49 +0300

    5

--- src/ru/javatutor/Exercise.java
+++ src/ru/javatutor/Exercise.java
@@ -2,9 +2,7 @@ package ru.javatutor;
 
 public class Exercise {
     public int[] makeMiddle(int[] nums) {
-        int[] numsMiddle = new int[2];
-        numsMiddle[0] = nums[(nums.length - 1) / 2];
-        numsMiddle[1] = nums[(nums.length - 1) / 2 + 1];
-        return numsMiddle;
+        int middleIndex = nums.length / 2;
+        return new int[]{nums[middleIndex - 1], nums[middleIndex]};
     }
 }
