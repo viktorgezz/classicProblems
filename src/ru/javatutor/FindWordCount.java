125d30ec28daca5a9014a5e62e34f399ab8a2f19
AndreyVerov <viktorgezz@mail.ru>
2023-07-28 12:47:04 +0300

    9

--- src/ru/javatutor/FindWordCount.java
+++ src/ru/javatutor/FindWordCount.java
@@ -6,12 +6,8 @@ import java.util.Map;
 public class FindWordCount {
     public Map<String, Integer> wordCount(String[] strings) {
         Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < strings.length; i++) {
-            if (map.containsKey(strings[i])) {
-                map.put(strings[i], map.get(strings[i]) + 1);
-            } else {
-                map.put(strings[i], 1);
-            }
+        for (String s : strings) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
         }
         return map;
     }
